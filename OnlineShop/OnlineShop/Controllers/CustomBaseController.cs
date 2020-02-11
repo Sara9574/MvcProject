fdc01c00a2c54a6aecbd307e829f985ee332726e
Sara9574 <sara.ghm95@gmail.com>
2020-02-11 20:44:58 +0330

    base controller is added

---
+++ OnlineShop/OnlineShop/Controllers/CustomBaseController.cs
@@ -0,0 +1,28 @@
+﻿using OnlineShop.Models.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineShop.Controllers
+{
+    public class CustomBaseController : Controller
+    {
+        public int CurrentUserId { get; set; }
+        public string CurrentUserFullname { get; set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var exists = filterContext.HttpContext.Request.Cookies.AllKeys.Contains(FormsAuthentication.FormsCookieName);
+            if (exists)
+            {
+                var cookieItem = Request.Cookies[FormsAuthentication.FormsCookieName];
+                var ticket = FormsAuthentication.Decrypt(cookieItem.Value);
+                CurrentUserId = int.Parse(ticket.UserData);
+                CurrentUserFullname = ticket.Name;
+            }
+        }
+    }
+}
