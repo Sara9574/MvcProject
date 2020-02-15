e9fe2618e225d303eb90811d1be44c1c96c4ff37
Sara9574 <sara.ghm95@gmail.com>
2020-02-15 21:46:51 +0330

    item page is added

--- OnlineShop/OnlineShop/Controllers/Mvc/SubCategoryController.cs
+++ OnlineShop/OnlineShop/Controllers/Mvc/SubCategoryController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using OnlineShop.Models;
+using OnlineShop.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +13,21 @@ namespace OnlineShop.Controllers.Mvc
     public class SubCategoryController : Controller
     {
         // GET: SubCategory
-        public ActionResult Items(int id)
+        public async Task<ActionResult> Items(int id)
         {
-            ViewBag.Test = id;
-            return View();
+            using (var db = new OnlineShopDbContext())
+            {
+                var result = await db.Items.Where(x => x.SubCategoryId == id).Select(x => new ItemViewModel {
+                    CatTitle = x.SubCategory.Category.Title,
+                    Color = x.Color.Title,
+                    Description = x.Desciption,
+                    Title = x.Title,
+                    Link = x.Image.Link,
+                    Price = x.Price,
+                    SubCatTitle = x.SubCategory.Title
+                }).ToListAsync();
+                return View(result);
+            } 
         }
     }
 }
---
+++ OnlineShop/OnlineShop/ViewModels/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.ViewModels
+{
+    public class ItemViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CatTitle { get; set; }
+        public string SubCatTitle { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Link { get; set; }
+        public int Price { get; set; }
+    }
+}
