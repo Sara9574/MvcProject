8e1bd848a4e64a2a17ee792091c8b3c09663d180
Sara9574 <sara.ghm95@gmail.com>
2020-02-22 22:40:22 +0330

    add and remove btn functions are added

--- OnlineShop/OnlineShop/Scripts/item/items.js
+++ OnlineShop/OnlineShop/Scripts/item/items.js
@@ -1,7 +1,15 @@
 ﻿$(document).ready(function () {
     $(".add-btn").on('click', function (e) {
-        let current = $("-count").text();
+        let current = $(this).closest('div').find('span').text()
         let currentCount = parseInt(current);
-        $(".count").text(currentCount+1);
+        $(this).closest('div').find('span').text(currentCount + 1)
+    });
+
+    $(".remove-btn").on('click', function (e) {
+        let current = $(this).closest('div').find('span').text()
+        let currentCount = parseInt(current);
+        if (currentCount > 0) {
+            $(this).closest('div').find('span').text(currentCount - 1)
+        }
     });
 });
