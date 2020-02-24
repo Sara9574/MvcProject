bf1d5508e1eb92ef2c1ffc7f353e30567babda18
Sara9574 <sara.ghm95@gmail.com>
2020-02-24 23:47:51 +0330

    bug fix in add and remove to cart

--- OnlineShop/OnlineShop/Scripts/item/items.js
+++ OnlineShop/OnlineShop/Scripts/item/items.js
@@ -14,7 +14,7 @@
     })
 
     $(".add-btn").on('click', function (e) {
-        let current = $(this).closest('div').find('span').text();
+        var current = $(this).closest('div').find('span').text();
 
         $.ajax({
             type: "POST",
@@ -26,14 +26,15 @@
             .always(function () {
                 $.get("/cart/Count", function (data, status) {
                     $("#cart-count").text(data);
-                    let currentCount = parseInt(current);
-                    $(this).closest('div').find('span').text(currentCount + 1)
+
                 })
             });;
+        let currentCount = parseInt(current);
+        $(this).closest('div').find('span').text(currentCount + 1)
     });
 
     $(".remove-btn").on('click', function (e) {
-        let current = $(this).closest('div').find('span').text();
+        var current = $(this).closest('div').find('span').text();
         $.ajax({
             type: "POST",
             url: '/cart/remove',
@@ -44,11 +45,12 @@
             .always(function () {
                 $.get("/cart/Count", function (data, status) {
                     $("#cart-count").text(data);
-                    let currentCount = parseInt(current);
-                    if (currentCount > 0) {
-                        $(this).closest('div').find('span').text(currentCount - 1)
-                    }
+
                 })
             });;
+        let currentCount = parseInt(current);
+        if (currentCount > 0) {
+            $(this).closest('div').find('span').text(currentCount - 1)
+        }
     });
 });
