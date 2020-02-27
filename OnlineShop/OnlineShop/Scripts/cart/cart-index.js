98d75da69244d877bdf2c7d8220debefe7cdd2cf
Sara9574 <sara.ghm95@gmail.com>
2020-02-27 11:03:40 +0330

    add and remove btn in cart functions are added

--- OnlineShop/OnlineShop/Scripts/cart/cart-index.js
+++ OnlineShop/OnlineShop/Scripts/cart/cart-index.js
@@ -1,13 +1,11 @@
 ﻿$(document).ready(function () {
 
     $.get("/cart/FactorInfo", function (data, status) {
-        console.log(data);
         $("#total").text(data.Total);
         $("#sum").text(data.Sum);
         $("#discount").text(data.Discount);
-        $("#delivery").text(data.delivery);
+        $("#delivery").text(data.Delivery);
     })
-
 
     if ($("#item-wrapper").length != 0) {
         $(".factor").show();
@@ -17,18 +15,55 @@
     }
 
 
-
     $(".add-btn").on('click', function (e) {
         var current = $(this).closest('div').find('span').text();
-
         $.ajax({
             type: "POST",
             url: '/cart/Add',
             data: { id: $(this).val() },
         }).fail(function () {
             alert("error");
-        })
-            .always(function () {
+        }).always(function () {
+            $.get("/cart/FactorInfo", function (data, status) {
+                console.log(data);
+                $("#total").text(data.Total);
+                $("#sum").text(data.Sum);
+                $("#discount").text(data.Discount);
+                $("#delivery").text(data.Delivery);
+            })
+            $.get("/cart/Count", function (data, status) {
+                if (data > 0) {
+                    $("#cart-count").addClass("cart-count");
+                    $("#cart-count").text(data)
+                }
+                else {
+                    $("#cart-count").removeClass("cart-count");
+                }
+            })
+        });
+        let currentCount = parseInt(current);
+        $(this).closest('div').find('span').text(currentCount + 1);
+        let eachPrice = $(this).closest('div').parent().find("#each-price").text();
+        $(this).closest('div').parent().find("#total-price").text((currentCount + 1) * eachPrice);
+    });
+
+    $(".remove-btn").on('click', function (e) {
+        var current = $(this).closest('div').find('span').text();
+        let currentCount = parseInt(current);
+        if (currentCount > 0) {
+            $.ajax({
+                type: "POST",
+                url: '/cart/remove',
+                data: { id: $(this).val() },
+            }).fail(function () {
+                return alert("error");
+            }).always(function () {
+                $.get("/cart/FactorInfo", function (data, status) {
+                    $("#total").text(data.Total);
+                    $("#sum").text(data.Sum);
+                    $("#discount").text(data.Discount);
+                    $("#delivery").text(data.Delivery);
+                });
                 $.get("/cart/Count", function (data, status) {
                     if (data > 0) {
                         $("#cart-count").addClass("cart-count");
@@ -38,48 +73,20 @@
                         $("#cart-count").removeClass("cart-count");
                     }
                 })
-            });;
-        let currentCount = parseInt(current);
-        $(this).closest('div').find('span').text(currentCount + 1)
-    });
-
-    $(".remove-btn").on('click', function (e) {
-        var current = $(this).closest('div').find('span').text();
-        let currentCount = parseInt(current);
-
-        if (currentCount > 0) {
-            $.ajax({
-                type: "POST",
-                url: '/cart/remove',
-                data: { id: $(this).val() },
-            }).fail(function () {
-                return alert("error");
-            })
-                .always(function () {
-                    $.get("/cart/Count", function (data, status) {
-                        if (data > 0) {
-                            $("#cart-count").addClass("cart-count");
-                            $("#cart-count").text(data)
-                        }
-                        else {
-                            $("#cart-count").removeClass("cart-count");
-                        }
-                    })
-                });;
+            });
             if (currentCount != 1) {
-                $(this).closest('div').find('span').text(currentCount - 1)
+                $(this).closest('div').find('span').text(currentCount - 1);
+                let eachPrice = $(this).closest('div').parent().find("#each-price").text();
+                $(this).closest('div').parent().find("#total-price").text((currentCount - 1) * eachPrice);
             }
-            if (currentCount == 1) {
+            else {
                 $(this).closest('div').parent().remove();
-                console.log($("#item-wrapper").length);
                 if ($("#item-wrapper").length == 0) {
                     $(".factor").hide();
                     $("#empty").show();
                 }
             }
         }
-       
     });
 
-
-})
+});
