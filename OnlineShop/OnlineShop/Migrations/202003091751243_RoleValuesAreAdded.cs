cb486383d2096a32fd2bc82f1f971cbb19b6a004
Sara9574 <sara.ghm95@gmail.com>
2020-03-09 21:52:40 +0330

    Role is added

---
+++ OnlineShop/OnlineShop/Migrations/202003091751243_RoleValuesAreAdded.cs
@@ -0,0 +1,19 @@
+﻿namespace OnlineShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RoleValuesAreAdded : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"insert into role(id, title, tag) 
+                  values (1, N'کاربر', 'User'),
+                         (2, N'ادمین', 'Admin')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/Models/Tables/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models.Tables
+{
+    [Table(nameof(Role))]
+    public class Role : BaseLookUpTable
+    {
+    }
+}
