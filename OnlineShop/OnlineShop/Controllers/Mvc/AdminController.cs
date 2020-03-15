69a539e378d97276fbe38047570ff81e30a41f7a
Sara9574 <sara.ghm95@gmail.com>
2020-03-15 17:45:44 +0330

    error controller is added

--- OnlineShop/OnlineShop/Controllers/Mvc/AdminController.cs
+++ OnlineShop/OnlineShop/Controllers/Mvc/AdminController.cs
@@ -14,50 +14,113 @@ namespace OnlineShop.Controllers.Mvc
     {
         public ActionResult Panel()
         {
-            using (var db = new OnlineShopDbContext())
+            if (CurrentUserRoleId == 2)
             {
-                PanelViewModel model = new PanelViewModel();
-                model.UnVerifiedFeedbackCount = 4;
-                model.VerifiedUserCount = 10;
-                model.UnVerifiedUserCount = 3;
-                model.ProductCount = 30;
-                return View(model);
+                using (var db = new OnlineShopDbContext())
+                {
+                    PanelViewModel model = new PanelViewModel();
+                    model.UnVerifiedFeedbackCount = 4;
+                    model.VerifiedUserCount = 10;
+                    model.UnVerifiedUserCount = 3;
+                    model.ProductCount = 30;
+                    return View(model);
+                }
+            }
+            else
+            {
+                return Redirect("/error/unathorized");
             }
         }
         public ActionResult UserManagement()
         {
-            using (var db = new OnlineShopDbContext())
+            if (CurrentUserRoleId == 2)
             {
-                var list = db.Users.Where(x => x.IsVerified == false).ToList();
-                return View(list);
+                using (var db = new OnlineShopDbContext())
+                {
+                    var list = db.Users.Where(x => x.IsVerified == false).ToList();
+                    return View(list);
+                }
+            }
+            else
+            {
+                return Redirect("/error/unathorized");
             }
         }
 
         public ActionResult Products()
         {
-            using (var db = new OnlineShopDbContext())
+            if (CurrentUserRoleId == 2)
             {
-                var list = db.Items.Select(x => new ProductViewModel
+                using (var db = new OnlineShopDbContext())
                 {
-                    Category = x.SubCategory.Category.Title,
-                    Link = db.Images.Where(y => y.ItemId == x.Id && y.IsMain == true).Select(y => y.Link).FirstOrDefault(),
-                    Price = x.Price,
-                    SubCat = x.SubCategory.Title,
-                    Title = x.Title,
-                    ShowOnSite = x.ShowOnSite == true ? "بله" : "خیر",
-                    Id = x.Id
-                }).ToList();
-                return View(list);
+                    var list = db.Items.Select(x => new ProductViewModel
+                    {
+                        Category = x.SubCategory.Category.Title,
+                        Link = db.Images.Where(y => y.ItemId == x.Id && y.IsMain == true).Select(y => y.Link).FirstOrDefault(),
+                        Price = x.Price,
+                        SubCat = x.SubCategory.Title,
+                        Title = x.Title,
+                        ShowOnSite = x.ShowOnSite == true ? "بله" : "خیر",
+                        Id = x.Id
+                    }).ToList();
+                    return View(list);
+                }
+            }
+            else
+            {
+                return Redirect("/error/unathorized");
             }
         }
 
         public ActionResult AddProduct()
         {
-            using (var db = new OnlineShopDbContext())
+            if (CurrentUserRoleId == 2)
             {
-                ViewBag.Colors = JsonConvert.SerializeObject(db.Colors.Select(x => new { x.Id, x.ColorCode, x.Title }).ToList());
-                ViewBag.Sizes = JsonConvert.SerializeObject(db.Sizes.Select(x => new { x.Id, x.Tag, x.Title }).ToList());
-                return View();
+                using (var db = new OnlineShopDbContext())
+                {
+                    ViewBag.Colors = JsonConvert.SerializeObject(db.Colors.Select(x => new { x.Id, x.ColorCode, x.Title }).ToList());
+                    ViewBag.Sizes = JsonConvert.SerializeObject(db.Sizes.Select(x => new { x.Id, x.Tag, x.Title }).ToList());
+                    return View();
+                }
+            }
+            else
+            {
+                return Redirect("/error/unathorized");
+            }
+        }
+
+        public ActionResult VerifyUser(int id)
+        {
+            if (CurrentUserRoleId == 2)
+            {
+                using (var db = new OnlineShopDbContext())
+                {
+                    var user = db.Users.Find(id);
+                    user.IsVerified = true;
+                    return Json(user);
+                }
+            }
+            else
+            {
+                Response.StatusCode = 401;
+                return Json(new { });
+            }
+        }
+        public ActionResult RejectUser(int id)
+        {
+            if (CurrentUserRoleId == 2)
+            {
+                using (var db = new OnlineShopDbContext())
+                {
+                    var user = db.Users.Find(id);
+                    user.IsVerified = false;
+                    return Json(user);
+                }
+            }
+            else
+            {
+                Response.StatusCode = 401;
+                return Json(new { });
             }
         }
     }
---
+++ OnlineShop/OnlineShop/Controllers/Mvc/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers.Mvc
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Unathorized()
+        {
+            return View();
+        }
+    }
+}
