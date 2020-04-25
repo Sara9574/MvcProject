e9bf04a96b5e1d67c99f9c229ad3f6f7a638d010
Sara9574 <sara.ghm95@gmail.com>
2020-04-25 10:06:11 +0430

    sub cat name fix

---
+++ OnlineShop/OnlineShop/Migrations/202004241838054_altercatnames1.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class altercatnames1 : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"update SubCategory
+                set Title=N'شلوار' where id=6");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
